c2916883a99001627e1f0c615a9983f1dee9d34f
karlg(pc) <karlgks@outlook.dk>
2023-10-16 17:57:54 +0200

    ting

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 
 # Import libraries
 from Structure import StructureClass
+from Hitboxes import
 import math
 import pygame
 pygame.init()
@@ -19,14 +20,16 @@ pygame.display.set_caption('placeholder title')
 
 # tiangna dnnasdnas das nasdn asdn nasdn wannds adnwandn nwan n d'
 structure = StructureClass(5, gameWindow)
-
+hitbox =
 
 # Load images
 graveyard = pygame.image.load('Graphics/graveyard.png')
-graveyard = pygame.transform.scale(graveyard, (1000, 1000))
+graveyard = pygame.transform.scale(graveyard, (500, 500))
+graveyardRect = graveyard.get_rect()
 
-
-
+xMoved = 0
+yMoved = 0
+movementSpeed = 2
 
 
 
@@ -38,8 +41,55 @@ while Running:
 
     gameWindow.fill('blue')
 
-    structure.move()
-    structure.draw_structure(graveyard)
+
+
+
+
+
+
+
+    wMoved = 0
+    aMoved = 0
+    sMoved = 0
+    dMoved = 0
+
+    keyPressed = pygame.key.get_pressed()
+
+
+
+    if keyPressed[pygame.K_w]:
+        wMoved = movementSpeed
+
+    if keyPressed[pygame.K_a]:
+        aMoved = movementSpeed
+
+    if keyPressed[pygame.K_s]:
+        sMoved = -movementSpeed
+
+    if keyPressed[pygame.K_d]:
+        dMoved = -movementSpeed
+
+    if wMoved and aMoved != 0:
+        wMoved = math.sin(45) * movementSpeed
+        aMoved = math.sin(45) * movementSpeed
+
+    if wMoved and dMoved != 0:
+        wMoved = math.sin(45) * movementSpeed
+        dMoved = -math.sin(45) * movementSpeed
+
+    if sMoved and aMoved != 0:
+        sMoved = -math.sin(45) * movementSpeed
+        aMoved = math.sin(45) * movementSpeed
+
+    if sMoved and dMoved != 0:
+        sMoved = -math.sin(45) * movementSpeed
+        dMoved = -math.sin(45) * movementSpeed
+
+
+    structure.move(wMoved, aMoved, sMoved, dMoved)
+    structure.draw(graveyard)
+
+    pygame.draw.rect(gameWindow, ('red'), pygame.Rect(800, 450, 10, 10))
 
     # gameWindow.blit(graveyard, (structure.xPos, structure.yPos))
 
--- Structure.py
+++ Structure.py
@@ -1,7 +1,6 @@
 import math
-
 import pygame
-
+#  ?????????????????????????????????????????????
 
 class StructureClass:
     def __init__(self, assetNumber, gameWindow):
@@ -12,48 +11,11 @@ class StructureClass:
         self.width = 50
         self.height = 50
 
-    def move(self):
-        movementSpeed = 5
-        wMovement = 0
-        aMovement = 0
-        sMovement = 0
-        dMovement = 0
-        keyPressed = pygame.key.get_pressed()
+    def move(self, wMoved, aMoved, sMoved, dMoved):
+        self.xPos += aMoved + dMoved
+        self.yPos += wMoved + sMoved
 
-        if keyPressed[pygame.K_w]:
-            wMovement = movementSpeed
-
-        if keyPressed[pygame.K_a]:
-            aMovement = movementSpeed
-
-        if keyPressed[pygame.K_s]:
-            sMovement = -movementSpeed
-
-        if keyPressed[pygame.K_d]:
-            dMovement = -movementSpeed
-
-        if keyPressed[pygame.K_w] and keyPressed[pygame.K_a]:
-            wMovement = math.sin(45) * movementSpeed
-            aMovement = math.sin(45) * movementSpeed
-
-        if keyPressed[pygame.K_w] and keyPressed[pygame.K_d]:
-            wMovement = math.sin(45) * movementSpeed
-            dMovement = -math.sin(45) * movementSpeed
-
-        if keyPressed[pygame.K_s] and keyPressed[pygame.K_a]:
-            sMovement = -math.sin(45) * movementSpeed
-            aMovement = math.sin(45) * movementSpeed
-
-        if keyPressed[pygame.K_s] and keyPressed[pygame.K_d]:
-            sMovement = -math.sin(45) * movementSpeed
-            dMovement = -math.sin(45) * movementSpeed
-
-        self.xPos += aMovement + dMovement
-        self.yPos += wMovement + sMovement
-
-
-
-    def draw_structure(self, graveyard):
+    def draw(self, graveyard):
         self.gameWindow.blit(graveyard, (self.xPos, self.yPos))
 
 
